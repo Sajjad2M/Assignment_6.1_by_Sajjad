b394c3f73a0da05b5c85e3fb1a96e7ee8b411ddf
Sajjad2M <sajjad57080@gmail.com>
2026-02-26 16:31:20 +0600

    add all changes and functions and adjust all designs

--- swiftCart.js
+++ swiftCart.js
@@ -20,12 +20,19 @@ let allPosts = () => {
 let allPosts2 = (api) =>{
     api.forEach(w =>{
         
-    })
+    }) 
+    //filter [...]
    
     api.forEach(e => {
-       
+        let q = e.category
+        
+        
+        // if(q === 'kid'){
+        //   api.filter
+        // }
+    
         let div1 = document.createElement('div')
-        div1.innerHTML = `<div class=" bg-white  rounded-[24px] border-[1px] border-b-slate-500/50 shadow-lg ">
+        div1.innerHTML = `<div class=" bg-white  rounded-[24px] border-[1px] border-b-slate-500/50 shadow-lg relative">
             <div class="px-8" id="">
                 <img src="${e.image}" alt="" class="lg:w-[420px] h-[250px] p-5">
             </div>
@@ -44,12 +51,14 @@ let allPosts2 = (api) =>{
             <h1 class="bg-green-100 px-4 font-extrabold text-[20px] pt-0">$${e.price}</h1>
             <!-- 2 Buttonss -->
             <div class="flex gap-4 bg-green-100 px-4 pb-4 font-bold text-center pt-4 rounded-b-[24px]" id="">
-                 <button class=" w-full bg-white shadow-lg lg:py-[5px] lg:px-7 lg:rounded-[5px] " id="btn1">Details</button>
+                 <button class=" w-full bg-white shadow-lg lg:py-[5px] lg:px-7 lg:rounded-[5px] " id="btn10">Details</button>
 
                  <button class="w-full bg-green-500 shadow-lg text-white lg:py-[5px] lg:px-7 lg:rounded-[5px]" id="btn2">Add</button>
              
             </div>
-        
+            <div class="absolute top-0 h-auto mx-2 mt-[20px] border-t-8  border-green-400 p-4 bg-white rounded-[16px] box-border shadow-2xl bg-opacity-90 hidden" id="details3">
+        <div class="" id="details4"></div>
+        </div>
            </div>
            `
         display1.appendChild(div1)
@@ -82,8 +91,8 @@ let allPosts2 = (api) =>{
                  <button class="w-full bg-green-500 shadow-lg text-white lg:py-[5px] lg:px-7 lg:rounded-[5px]" id="btn2">Add</button>
              
             </div>
-        <div class="absolute bottom-0 mx-4 mb-[60px] border-[1px] border-red-400 bg-[#74ccf4] rounded-[16px] box-border shadow-2xl ">
-        <div class=" hidden p-5" id="details1"></div>
+        <div class="absolute top-0 mx-4 mt-[30px] border-l-8 border-green-500 bg-white rounded-[16px] box-border shadow-2xl hidden bg-opacity-90 " id="details2">
+        <div class=" p-5" id="details1"></div>
         </div>
            </div>
            </div>
@@ -91,39 +100,70 @@ let allPosts2 = (api) =>{
             display2.appendChild(div5)
             let btn4 = div5.querySelector('#btn1')
             let div6 = div5.querySelector('#details1')
+            let div7 = div5.querySelector('#details2')
              let details2 = document.createElement('div') 
+           
              let i = 1;
-             let i2 = 0;
-            fullbody.addEventListener('click', ()=>{
-                i2 = i
-                console.log(i2)
-             
-        })
+            
             btn4.addEventListener('click', () =>{
                 i++
                details2.innerHTML=''
               //div6.classList.remove('hidden')
               if(i % 2 === 0){
-                div6.classList.remove('hidden')
+                div7.classList.remove('hidden')
+               
+              }else{
+                 div7.classList.add('hidden')
+                
               }
               
               details2.innerHTML = `
-                <h1 class="">Title: ${e.title}</h1>
-                <h1 class="">Description: ${e.description}</h1>
-                <h1 class="">Rating: ${e.title}</h1>
-                <h1 class="">Price: ${e.price}</h1>
+                <h1 class="text-center text-[22px] font-bold text-green-600 "> ${e.title}</h1>
+                <h1 class="text-left text-[20px] font-semibold mt-2">Description: ${e.description}</h1>
+                <h1 class="text-left text-[20px] font-bold mt-1"><span class="text-[18px] font-bold">Ratings:</span> ${e.rating}</h1>
+                <h1 class="text-left text-[20px] font-bold"><span class="text-[18px] font-bold mt-1 ">Price: </span>$${e.price}</h1>
                 `
                  
                 div6.appendChild(details2)
+              
+            
                 //console.log(i)
                 
              })
-      
+          
         }
        
-    
-
-       
+         let btn5 = div1.querySelector('#btn10')
+         let div9 = div1.querySelector('#details3')
+         let div8 = div1.querySelector('#details4')
+         let v = 1;
+        let details3 = document.createElement('div') 
+         btn5.addEventListener('click', () =>{
+                v++
+               details3.innerHTML=''
+              //div6.classList.remove('hidden')
+              if(v % 2 === 0){
+             
+                div9.classList.remove('hidden')
+                //div8.classList.add('bg-red-600')
+              }else{
+                
+                 div9.classList.add('hidden')
+              }
+              
+              details3.innerHTML = `
+                <h1 class="text-center text-[20px] font-bold text-green-600 "> ${e.title}</h1>
+                <h1 class="text-left text-[20px] font-semibold mt-2">Description: ${e.description}</h1>
+                <h1 class="text-left text-[20px] font-bold mt-1"><span class="text-[18px] font-bold">Ratings:</span> ${e.rating}</h1>
+                <h1 class="text-left text-[20px] font-bold"><span class="text-[18px] font-bold mt-1 ">Price: </span>$${e.price}</h1>
+                `
+                 
+                div9.appendChild(details3)
+            
+             console.log(v)
+                
+             })
+      
         
     //    let btn4 = div5.querySelector('#btn1')
     //     //  <div class="absolute top-0 bg-red-300 my-10">
